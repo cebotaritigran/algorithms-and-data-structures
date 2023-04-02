0bcfcdd8358cf6ce28031383e66750ee4b5f470a
CebotariTigran <cebotaritigran@gmail.com>
2023-04-02 23:43:26 +0200

    Add nodes

--- linked-list/linkedList.js
+++ linked-list/linkedList.js
@@ -1,3 +1,24 @@
 class LinkedList {
-    
+
+    class Node {
+    constructor(data, next) {
+        this.data = data
+        this.next = next
+    }
 }
+
+const test = () => {
+    let list = []
+    let array = new LinkedList([])
+    let data = "1";
+    let node2 = new Node(data, null)
+    let node = new Node(data, node2)
+
+    console.log(node);
+}
+return test;
+}
+
+
+
+console.log(test());
