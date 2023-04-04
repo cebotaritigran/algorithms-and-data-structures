a0a76d78b0e21a24286604a612b9bd122ba40956
CebotariTigran <cebotaritigran@gmail.com>
2023-04-04 11:27:04 +0200

    Add append function to linked list

--- linked-list/linkedList.js
+++ linked-list/linkedList.js
@@ -1,24 +1,34 @@
 class LinkedList {
+    constructor(head = null) {
+        this.head = head
+    }
 
-    class Node {
-    constructor(data, next) {
+    append(node1, node2) {
+        node1.next = node2
+        return node1
+    }
+
+}
+
+class Node {
+    constructor(data = null, next = null) {
         this.data = data
         this.next = next
     }
 }
 
 const test = () => {
-    let list = []
-    let array = new LinkedList([])
-    let data = "1";
-    let node2 = new Node(data, null)
-    let node = new Node(data, node2)
 
-    console.log(node);
+    let list = new Node("1", null)
+    let list2 = new Node("2", null)
+    let list3 = new Node("3", null)
+
+    let linkedlist = new LinkedList(list)
+
+    linkedlist.append(list, list2)
+    linkedlist.append(list2, list3)
+
+    return linkedlist;
 }
-return test;
-}
-
-
 
 console.log(test());
