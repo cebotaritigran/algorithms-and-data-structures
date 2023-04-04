e27e59c32d64d6a0a8543e55414c297c2b607614
CebotariTigran <cebotaritigran@gmail.com>
2023-04-04 13:59:20 +0200

    Add prepand, size, head, tail, at functions

--- linked-list/linkedList.js
+++ linked-list/linkedList.js
@@ -3,11 +3,87 @@ class LinkedList {
         this.head = head
     }
 
-    append(node1, node2) {
-        node1.next = node2
-        return node1
+    append(value) {
+        // creating new node with value
+        let newNode = new Node(value, null)
+        //if the head of the list is null creating new head node with value
+        if (this.head == null) {
+            this.head = new Node(value, null)
+            return;
+        }
+        // if the head value is not null
+        //setting newnode next value to null
+        newNode.next = null;
+
+        //creating tail node
+        let tail = this.head;
+        //iterating through linked list to find tail node
+        while (tail.next != null) {
+            //setting the node to next node
+            tail = tail.next;
+        }
+        //setting the last node to newNode
+        tail.next = newNode
+        return;
     }
 
+    prepend(value) {
+        let newNode = new Node(value, null)
+        let tmp = this.head
+        this.head = newNode
+        let tail = this.head;
+        //iterating through linked list to find tail node
+        while (tail.next != null) {
+            //setting the node to next node
+            tail = tail.next;
+        }
+        //setting the last node to tmp
+        tail.next = tmp
+        return;
+    }
+
+    size() {
+        let tail = this.head;
+        let numberOfNodes = 1;
+        while (tail.next != null) {
+            //setting the node to next node
+            numberOfNodes++;
+            tail = tail.next;
+        }
+        return numberOfNodes;
+    }
+
+    head() {
+        let firstNode = this.head
+        return firstNode;
+    }
+
+    tail() {
+        let tailOfList = this.head;
+        while (tailOfList.next != null) {
+            //setting the node to next node
+            tailOfList = tailOfList.next;
+        }
+        return tailOfList;
+    }
+
+    at(index) {
+        let tail = this.head;
+        let numberOfNodes = 1;
+
+        while (tail.next != null) {
+            //setting the node to next node
+            numberOfNodes++;
+            tail = tail.next;
+            if (index == numberOfNodes) {
+                return tail;
+            }
+        }
+    }
+
+    pop() {
+        
+    }
 }
 
 class Node {
@@ -18,17 +94,13 @@ class Node {
 }
 
 const test = () => {
-
-    let list = new Node("1", null)
-    let list2 = new Node("2", null)
-    let list3 = new Node("3", null)
-
-    let linkedlist = new LinkedList(list)
-
-    linkedlist.append(list, list2)
-    linkedlist.append(list2, list3)
-
-    return linkedlist;
+    let linkedlist = new LinkedList(null)
+    linkedlist.append("0")
+    linkedlist.prepend("1")
+    linkedlist.prepend("3")
+    linkedlist.prepend("4")
+    linkedlist.prepend("6")
+    return linkedlist.at(2);
 }
 
 console.log(test());
