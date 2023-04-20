819031a8ae0c27d752b4bd18e5894335149d0ca0
CebotariTigran <cebotaritigran@gmail.com>
2023-04-20 22:08:09 +0200

    Add a few more test for capitalize function

--- test-driven-development-jest/capitalize.js
+++ test-driven-development-jest/capitalize.js
@@ -1,6 +1,17 @@
 export const capitalize = (string) => {
-    console.log(string)
-    return "Hey"
+    let slicedString = "";
+    console.log(string.length)
+    for (let i = 0; i < string.length; i++) {
+        if (parseInt(string.charAt(i)) == Number) {
+            slicedString += string.slice(i)
+            console.log(string.slice(i))
+        }
+    }
+
+    let capatalizedString = slicedString + string.charAt(0).toUpperCase() + string.slice(1)
+    console.log(capatalizedString)
+    return capatalizedString
+
 }
 
 export default capitalize 
--- test-driven-development-jest/capitalize.test.js
+++ test-driven-development-jest/capitalize.test.js
@@ -1,5 +1,26 @@
 import capitalize from './capitalize';
 
-test('1', () => {
-  expect(capitalize("123")).toBe("Hey");
+test('Check if first letter is capatalized', () => {
+  expect(capitalize("test")).toBe("Test");
 });
+
+test('Check if first letter is capatalized', () => {
+  expect(capitalize("Test")).toBe("Test");
+});
+
+test('Check if first letter in a capatalized word is a capatalized word', () => {
+  expect(capitalize("TEST")).toBe("TEST");
+});
+
+test('Check if first letter in 2 word string is capatalized', () => {
+  expect(capitalize("test test")).toBe("Test test");
+});
+
+test('Check if first letter is capatalized in a word with numbers', () => {
+  expect(capitalize("test123")).toBe("Test123");
+});
+
+test('Check if first letter is capatalized in a word with numbers in front', () => {
+  expect(capitalize("123test")).toBe("123Test");
+});
+
