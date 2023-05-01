640bca0e83fc02bd38cb4718188f3cb339158837
CebotariTigran <cebotaritigran@gmail.com>
2023-05-01 05:51:40 -0400

    Finish ceasar Cipher and tests

--- test-driven-development-jest/caesarCipher.js
+++ test-driven-development-jest/caesarCipher.js
@@ -1,7 +1,25 @@
-export const cipher = (string) => {
- 
+export const cipher = (string, key) => {
 
-
+    let x = ""
+    let encryptedText = "";
+    let character = "";
+    for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) >= 97) {
+            x = string.charCodeAt(i) - 97;
+            x = (x + key) % 26;
+            character = String.fromCharCode(x + 97)
+        } else if (string.charCodeAt(i) >= 65) {
+            x = string.charCodeAt(i) - 65;
+            x = (x + key) % 26;
+            character = String.fromCharCode(x + 65)
+        } else if (string.charCodeAt(i) < 65 || string.charCodeAt(i) > 90 || string.charCodeAt(i) < 97 || string.charCodeAt(i) > 122) {
+            x = string.charCodeAt(i)
+            character = String.fromCharCode(x)
+        }
+        encryptedText += character
+    }
+    console.log(encryptedText)
+    return encryptedText;
 }
 
 export default cipher 
--- test-driven-development-jest/caesarCipher.test.js
+++ test-driven-development-jest/caesarCipher.test.js
@@ -1,5 +1,25 @@
 import cipher from './caesarCipher';
 
-test('Check if first letter is capatalized', () => {
-    expect(cipher("test")).toBe("Test");
+test('Check if text is encypted', () => {
+    expect(cipher("test", 1)).toBe("uftu");
+});
+
+test('Check if another text is encrypted', () => {
+    expect(cipher("hey", 1)).toBe("ifz");
+});
+
+test('Check if a text with z with a key of 1 becomes a', () => {
+    expect(cipher("zebra", 1)).toBe("afcsb");
+});
+
+test('Check if a text with z and some capital letters is encrypted', () => {
+    expect(cipher("zeBra", 1)).toBe("afCsb");
+});
+
+test('Check if a text with spaces is encrypted', () => {
+    expect(cipher("zeBra Jump", 1)).toBe("afCsb Kvnq");
+});
+
+test('Check if a text with spaces and special characters is encrypted', () => {
+    expect(cipher("zeBra Jump!!?", 1)).toBe("afCsb Kvnq!!?");
 });
