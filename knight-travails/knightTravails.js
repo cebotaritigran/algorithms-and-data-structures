c302d568443ba5eff308a25fee369a09af70d899
tigrancebotari <tigrancebotari@gmail.com>
2025-04-29 09:27:51 -0400

    update knight-travails

--- knight-travails/knightTravails.js
+++ knight-travails/knightTravails.js
@@ -1,40 +1,78 @@
-function Node(pos, path) {
-    if (pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) {
-        return null;
+function createBoard() {
+    let chessBoard = [];
+    for (let i = 0; i < 8; i++) {
+        chessBoard[i] = new Array(8);
     }
-    return { pos, path };
+    return chessBoard;
 }
 
-function knightMoves([x, y], [a, b]) {
-    let q = [Node([x, y], [[x, y]])];
-    let currentNode = q.shift();
+class Path {
+    constructor(currentPos, parentNode) {
+        this.currentPos = currentPos;
+        this.parentNode = parentNode
 
-
-    while (currentNode.pos[0] !== a || currentNode.pos[1] !== b) {
-        let moves = [
-            [currentNode.pos[0] + 2, currentNode.pos[1] - 1],
-            [currentNode.pos[0] + 2, currentNode.pos[1] + 1],
-            [currentNode.pos[0] - 2, currentNode.pos[1] - 1],
-            [currentNode.pos[0] - 2, currentNode.pos[1] + 1],
-            [currentNode.pos[0] + 1, currentNode.pos[1] - 2],
-            [currentNode.pos[0] + 1, currentNode.pos[1] + 2],
-            [currentNode.pos[0] - 1, currentNode.pos[1] - 2],
-            [currentNode.pos[0] - 1, currentNode.pos[1] + 2],
-        ];
-
-        moves.forEach((move) => {
-            let node = Node(move, currentNode.path.concat([move]));
-            if (node) {
-                q.push(node);
-            }
-        });
-        currentNode = q.shift();
     }
-    console.log(
-        `=> You made it in ${currentNode.path.length - 1} moves!  Here's your path:`
-    );
-    currentNode.path.forEach((pos) => {
-        console.log(pos);
-    });
 }
-knightMoves([3, 3], [4, 3]);
+
+// let chessBoard = [[0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+//possible moves
+// let x = [-2, -1, 1, 2, 2, 1, -2, -1];
+// let y = [1, 2, 2, 1, -1, -2, -1, -2];
+let possibleMoves = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-2, -1], [-1, -2]]
+
+function knightMoves(from, toWhere) {
+    // this function will take in two arrays, the first one is the starting position and the second one is the destination
+    // the function will return the number of moves it took to get to the destination
+    let dx = toWhere[0];
+    let dy = toWhere[1];
+    let count = 0;
+    let queue = [];
+    let firstNode = new Path(from, null, null)
+    queue.push(firstNode);
+
+    // the while loop will run until the first element in the queue is equal to the destination
+    // the first element in the queue is the current position of the knight
+    // the current position of the knight is the first element in the queue
+    // the destination is the second element in the queue
+    // the queue will be used to store the current position of the knight and the parent node
+    // the parent node will be used to trace back the path taken by the knight
+    while (queue[0].currentPos[0] !== dx || queue[0].currentPos[1] !== dy) {
+        let newPosition;
+
+        possibleMoves.forEach(move => {
+            // newPosition = [queue[0].currentPos[0] + move[0], queue[0].currentPos[1] + move[1]]
+            let tmp = new Path([queue[0].currentPos[0] + move[0], queue[0].currentPos[1] + move[1]], queue[0]);
+            queue.push(tmp);
+        })
+
+        queue.shift();
+    }
+
+    let tmp = queue[0];
+    let moves = []
+    // getting the path taken by the knight
+    // the path taken by the knight is the parent node of the current position of the knight
+    // the parent node will be used to trace back the path taken by the knight
+    while (tmp != null) {
+        console.log(tmp.currentPos);
+        moves.push(tmp.currentPos);
+        tmp = tmp.parentNode;
+        count++;
+    }
+    console.log("Path taken:");
+    moves.reverse().forEach(pos => console.log(pos));
+
+    console.log(`You made it in ${count} moves!`);
+}
+
+// let chessBoard = createBoard();
+knightMoves([0, 0], [3, 3])
